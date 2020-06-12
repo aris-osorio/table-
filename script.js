4d0c1a75a0e325c6e22eea8ba57959ccf2dca2a6
aris-osorio <aris.osorio@alumnos.udg.mx>
2020-06-11 19:37:29 -0500

    comentarios

--- script.js
+++ script.js
@@ -72,70 +72,87 @@ window.addUser = addUser
 
 //obtener valor del filtro seleccionado
 function getfiltervalue()
-{
+{   //obtengo elemento del filtro
     const inputfilter = document.getElementById("select-filter")
-    const fvalue = inputfilter.value 
+    //obtengo el texto seleccionado 
+    const fvalue = inputfilter.value
+    //retorno seleccion 
     return fvalue
 }
 //obtiene los usuarios filtrados
 function getfilterusers(filtervalue)
 {
+    //crear array
     var arrayfilter = []
+    // obtego datos de users
     switch (filtervalue) 
     {
+        //si es femenino
         case 'female':
+            //busco y filtro
             arrayfilter =  users.filter(function(user) 
                            {
                                return user.gender == 'female'
                            })
             break;
-
+        //correo academlo                   
         case 'academlo':
             arrayfilter = users.filter(function(user) 
                           {
+                            //obtengo el email  
                             var mail = user.email
-                           
+                           // genero direccion inicial substring
                             var caracter1 = mail.indexOf('@');
+                            //genero direccion final substring
                             var caracter2 = mail.indexOf('.');
-
+                            //indico que solo quiero el dominio del correo
                             var dominio = mail.substring(caracter1 + 1, caracter2);
-                        
+                            //retorno dominio que sea de academlo
                             return dominio == 'academlo'
                           })
             break;
-
+        // orden alfabetico 
         case 'sort':
             arrayfilter =  users.sort(function(a, b) 
                            {
+                               //primer valor a comparar
                                var user1 = a.name;
+                               //segundo valor a comparar
                                var user2 = b.name;
-
+                               // retorno el orden alfabetico de las dos opciones
                                return user1.localeCompare(user2)
                            })
             break;    
     }
-
+    //retorno tabla filtrada
     return arrayfilter
 }
 
 
 function filter() {
+    //obtengo el valor del filtro
     const filtervalue = getfiltervalue()
+    //genero tabla filtrada
     const filterusers = getfilterusers(filtervalue)
+    //imprimo tabla filtrada
     printUsers(filterusers)
 }
 window.filter = filter
-
+//remueve usuario
 function removeUser(id)
 {
+    //indico que elimine una posicion anterior al id ya que el array empieza de 0
+    //y que solo elimine 1 elemento
     users.splice(id-1, 1)
 }
-
+//funcion del boton donde paso id como parametro desde el button
 function removeButton(id)
 { 
-    alert("id = " + id)
+    //elimino usuario
     removeUser(id)
+    //ordeno id
     orderIDs()
+    //imprime usuarios de nuevo 
     printUsers(users)
 
 }
